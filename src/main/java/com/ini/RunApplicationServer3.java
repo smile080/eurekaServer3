71fcdf71763ddca0393b0186dd8334df508771e8
chen_jian <chenjian190000774@126.com>
2018-10-21 22:32:48 +0800

    2181021 22:31

--- src/main/java/com/ini/RunApplicationServer3.java
+++ src/main/java/com/ini/RunApplicationServer3.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class RunApplicationServer2 {
+public class RunApplicationServer3 {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(RunApplication.class, args);
-		new SpringApplicationBuilder(RunApplicationServer2.class).web(true).run(args);
+		new SpringApplicationBuilder(RunApplicationServer3.class).web(true).run(args);
 	}
 }
